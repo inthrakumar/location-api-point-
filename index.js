5d678230d26e696c778b36cf94f998f31910ae21
inthrakumar <inthrak04@gmail.com>
2024-07-29 15:14:00 +0530

    api point latitude longitude

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+  res.send('CORS configuration is set up!');
+});
+
+app.post('/api/ip-info', async (req, res) => {
+  try {
+    const { ip } = req.body;
+    const response = await axios.get(
+      `https://api.ip2location.io/?key=${process.env.LOCATION_API}&ip=${ip}&format=json`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

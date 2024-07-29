9f2eea669c19c38e3ca6f301830bc6c867051555
inthrakumar <inthrak04@gmail.com>
2024-07-29 16:08:37 +0530

    Cors update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };

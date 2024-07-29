b1ad9aa43eb6a14f9f87b180c4958ab03ab0b23f
inthrakumar <inthrak04@gmail.com>
2024-07-29 18:28:08 +0530

    update index.js

--- index.js
+++ index.js
@@ -3,19 +3,30 @@ import axios from 'axios';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+
 const app = express();
 
 const corsOptions = {
-  credentials: true,
-  optionSuccessStatus: 200,
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionSuccessStatus: 200,
 };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('CORS configuration is set up!');
@@ -29,7 +40,8 @@ app.post('/api/ip-info', async (req, res) => {
     );
     res.json(response.data);
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
